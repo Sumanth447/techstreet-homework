21921b7b3da679e0cd0cd6501e86b028e886f9d4
Sumanth <sumanth447.k@gmail.com>
2017-08-24 02:44:57 +0530

    apply validation

--- src/app/login.component.ts
+++ src/app/login.component.ts
@@ -1,24 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularService } from '../angular/service';
-import { FormBuilder, FormGroup } from '@angular/forms';
-import {LoginService} from './login.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { LoginService } from './login.service';
 
 @Component({
   selector: 'login-component',
-  templateUrl: './login.component.html'
+  templateUrl: './login.component.html',
+  styles:[`
+    .alert{
+      color:red;
+    }
+  `]
 })
 export class LoginComponent {
   loginForm: FormGroup;
-  loggedIn:  string = null;
-
+  loggedIn: string = null;
   constructor(
     private _service: AngularService,
     private _builder: FormBuilder,
-    private _loginService:LoginService
+    private _loginService: LoginService
   ) {
     this.loginForm = this._builder.group({
-      username: '',
-      password: ''
+      username: ['', Validators.required],
+      password: ['', Validators.required]
     });
   }
 
@@ -27,6 +31,6 @@ export class LoginComponent {
     this._service.setDirective(1);
   }
   save() {
-   this._loginService.login(this.loginForm.value.username,this.loginForm.value.password);
+    this._loginService.login(this.loginForm.value.username, this.loginForm.value.password);
   }
 }
